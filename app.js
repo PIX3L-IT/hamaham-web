c0568c204ada48b583061973f09a4d5aa6bd84db
Levi <Daz>
2025-04-02 17:38:59 -0600

    test: poner archivos básicos de aplicación

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files (optional, if you have CSS/JS)
+app.use(express.static("public"));
+
+// Serve an HTML page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "test.html"));
+});
+
+// Start server
+const PORT = 4000;
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
