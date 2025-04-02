c1dfa6912e163c34ccfdbd49e9e8050da0a13355
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2025-04-02 13:49:31 -0600

    feat: agregar colores adecuados a la marca

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const path = require('path');
-const expressLayouts = require('express-ejs-layouts'); // ⬅️ Aquí
+const expressLayouts = require('express-ejs-layouts'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuración EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(expressLayouts); // ⬅️ Usa layouts
-app.set('layout', 'layout'); // ⬅️ Define layout por default
+app.use(expressLayouts); 
+app.set('layout', 'layout'); 
 
-// Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
 app.get('/pacientes', (req, res) => {
     res.render(path.join(__dirname, 'pacientes/views/homepage.ejs'), {
       layout: 'layout',
