1a1482d1a12c3cd297972d1833c66c9d3b5cf00d
unknown <ViejoAgrio>
2025-04-03 13:18:33 -0600

    chore: agregar archivos para prueba firebase

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files (optional, if you have CSS/JS)
+app.use(express.static("public"));
+
+// Serve an HTML page
+app.get("/test", (req, res) => {
+    res.send("testing testing levi fanpage");
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
