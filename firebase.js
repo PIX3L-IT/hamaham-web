40b2b6aa13abb32b50bda4b8faefb4824cdfd9d6
Levi <Daz>
2025-04-07 21:56:01 -0600

    fix: cambiar firebase.js para tener datos admin

--- firebase.js
+++ firebase.js
@@ -1,23 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "@firebase/app";
-import { getAuth } from "@firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+var admin = require("firebase-admin");
 
-// Your web app's Firebase configuration
+var serviceAccount = require("./serviceAccountKey.json");
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-
-export { app, auth };
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
