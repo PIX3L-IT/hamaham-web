0ac2de155f2104785b3a563981a6aba9f227703b
Levi <Daz>
2025-04-08 08:55:07 -0600

    chore: mover variables firebase a archivo aparte

--- app.js
+++ app.js
@@ -49,6 +49,18 @@ app.get('/', (req, res) => {
   res.redirect('/pacientes/add-patient');
 });
 
+app.get('/js/firebase-config.js', (req, res) => {
+  res.type('application/javascript');
+  res.render('js/firebase-config', {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  });
+});
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
