8e1021b6c3cc2b753ae154a9d3deeb1844391981
Maxime-VP <111540703+Maxime-VP@users.noreply.github.com>
2025-04-11 19:21:04 -0600

    Revert "Test/v1.1.0/prueba mvc"

--- clinica/control/routes/clinica.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const clinicaController = require('../controllers/clinica.controller');
-
-// Ruta para el menú principal
-router.get('/menu', clinicaController.getMenu);
-
-module.exports = router;
